418ed3238347120660e23eb801bc527732e07a32
mpayne30 <142361747+mpayne30@users.noreply.github.com>
2023-09-21 13:16:58 -0500

    Add-Task partial implementation

---
+++ taskmaster/src/Add-Task.js
@@ -0,0 +1,35 @@
+
+import React from "react";
+
+function Add-Task() {
+  return (
+    <div className="Add-Task">
+      <header className = "Add-Task-Header">
+	  
+	  </header>
+	  
+	  <body className = "Add-Task-Body">
+          <form>
+            <label>
+              TaskName:
+              <input type="text" name="taskName" />
+            </label>
+
+            <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+
+            <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+
+            <button onclick="">Submit</button>
+          </form>
+	  </body>
+    </div>
+  );
+}
+
+export default Add-Task;
