3018ad132afd88f9fc2f17eb0332eac7b9f4d2f8
mpayne30 <142361747+mpayne30@users.noreply.github.com>
2023-09-26 12:40:34 -0500

    7.1 Implementation Task, TaskForm, and TaskList

---
+++ taskmaster/src/TaskList.js
@@ -0,0 +1,21 @@
+   import React from 'react';
+
+   const TaskList = ({ tasks, onTaskClick }) => (
+
+     <ul>
+
+       {tasks.map((task) => (
+
+         <li key={task.id} onClick={() => onTaskClick(task.id)}>
+
+           {task.title} - {task.completed ? 'Completed' : 'Pending'}
+
+         </li>
+
+       ))}
+
+     </ul>
+
+   );
+
+   export default TaskList;
